60a6b4fc83e7035dc9e2b5e0c19c4200263350b8
Joaquin <AAJ2025app@gmail.com>
2025-10-02 23:36:48 -0400

    Changed the title to Ciao

--- Ciao/ContentView.swift
+++ Ciao/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("wnigga")
+            Text("CIAO")
+                .font(.title)
+                .fontWeight(.bold)
+            Spacer()
         }
         .padding()
     }
