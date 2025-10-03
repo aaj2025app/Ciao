aef0a60a7185fd1db1b3ec1a384f7ccc70c6daae
Joaquin Vigil <AAJ2025app@gmail.com>
2025-10-02 23:02:28 -0400

    Initial Commit

---
+++ Ciao/CiaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiaoApp.swift
+//  Ciao
+//
+//  Created by Joaquin vigil on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CiaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
